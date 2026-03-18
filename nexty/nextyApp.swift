a182ea4dde7e9a1906a0f58785a0b6ba3c0d1a78
Tal Cohen <talthent@gmail.com>
2026-03-18 12:57:23 +0200

    Initial Commit

---
+++ nexty/nextyApp.swift
@@ -0,0 +1,17 @@
+//
+//  nextyApp.swift
+//  nexty
+//
+//  Created by Tal Cohen on 18/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct nextyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
